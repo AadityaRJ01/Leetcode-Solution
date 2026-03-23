3991568a23db1a3ed391afee8243795ed3bbfc49
Aaditya Raj Joshi <1ms23cs002@msrit.edu>
2026-03-23 18:54:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.91%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle = 2 * (n - 1);
+        time = time % cycle;
+
+        if (time < n) {
+            return 1 + time;
+        } else {
+            return n - (time - (n - 1));
+        }
+    }
+}
