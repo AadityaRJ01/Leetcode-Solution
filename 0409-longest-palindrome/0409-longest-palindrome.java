192fda06ddabaa88621c8784452a906b30f7b798
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-05-01 17:11:03 +0530

    Time: 1 ms (100%), Space: 43.2 MB (55.28%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq=new int[128];
+
+        for(char c:s.toCharArray()){
+            freq[c]++;
+        }
+        int length=0;
+        for(int count:freq){
+            length+=(count/2)*2;
+
+            if(length%2==0 && count%2==1){
+                length+=1;
+            }
+        }
+        return length;
+    }
+}
