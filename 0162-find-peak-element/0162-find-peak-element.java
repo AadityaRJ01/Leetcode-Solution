1e625783ed81e08c9f8c161f9e4238d42353dc26
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-05-07 19:18:32 +0530

    Time: 0 ms (100%), Space: 44.2 MB (55.6%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,7 +6,7 @@ class Solution {
         while (low < high) {
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
+            if (nums[mid] >= nums[mid + 1]) {
                 high = mid;
             } else {
                 low = mid + 1;
