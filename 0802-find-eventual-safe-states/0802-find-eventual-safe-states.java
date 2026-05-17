e6d535f074058b1ea895094f5afbbd5e138cff7a
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-05-17 16:05:32 +0530

    Time: 19 ms (52.67%), Space: 61.6 MB (65.09%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> reverseGraph = new ArrayList<>();
+        int[] outDegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            reverseGraph.add(new ArrayList<>());
+        }
+        for (int u = 0; u < n; u++) {
+            outDegree[u] = graph[u].length;
+            for (int v : graph[u]) {
+                reverseGraph.get(v).add(u);
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (outDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        boolean[] safe = new boolean[n];
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            safe[node] = true;
+            for (int prev : reverseGraph.get(node)) {
+                outDegree[prev]--;
+                if (outDegree[prev] == 0) {
+                    queue.offer(prev);
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (safe[i]) result.add(i);
+        }
+
+        return result;
+    }
+}
