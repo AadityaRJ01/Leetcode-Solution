423d0bac4e775cf687d227ffa1d581d0ae3e52e0
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-07-31 18:02:05 +0530

    Time: 1 ms (70.4%), Space: 43.4 MB (93.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,30 +1,35 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-        rDFS(root,0,res);
-        return res;
-    }
-    public void rDFS(TreeNode node,int level,List<Integer> res){
-        if(node==null) return;
-        if(res.size()==level){
-            res.add(node.val);
+
+        List<Integer> ans = new ArrayList<>();
+
+        if(root == null)
+            return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+
+        while(!q.isEmpty()){
+
+            int size = q.size();
+
+            for(int i = 0; i < size; i++){
+
+                TreeNode curr = q.poll();
+
+                if(i == size - 1)
+                    ans.add(curr.val);
+
+                if(curr.left != null)
+                    q.offer(curr.left);
+
+                if(curr.right != null)
+                    q.offer(curr.right);
+            }
         }
-        rDFS(node.right,level+1,res);
-        rDFS(node.left,level+1,res);
+
+        return ans;
     }
 }
