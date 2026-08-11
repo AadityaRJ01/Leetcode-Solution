a7630e42a7653fb204a6309b1a3bcab05456bd7f
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-11 15:21:51 +0530

    Time: 17 ms (50.31%), Space: 51 MB (55.79%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,51 +1,28 @@
 class Solution {
-
-    private int countLessEqual(int[][] matrix, int mid) {
-
-        int n = matrix.length;
-
-        int row = n - 1;
-        int col = 0;
-
-        int count = 0;
-
-        while (row >= 0 && col < n) {
-
-            if (matrix[row][col] <= mid) {
-
-                count += row + 1;
-                col++;
-
-            } else {
-
-                row--;
-            }
-        }
-
-        return count;
-    }
-
     public int kthSmallest(int[][] matrix, int k) {
 
         int n = matrix.length;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a, b) -> a[0] - b[0]
+        );
+        for (int i = 0; i < n; i++) {
+            minHeap.offer(new int[]{matrix[i][0], i, 0});
+        }
+        int ans = 0;
 
-        int low = matrix[0][0];
-        int high = matrix[n - 1][n - 1];
+        while (k-- > 0) {
+            int[] curr = minHeap.poll();
+            int value = curr[0];
+            int row = curr[1];
+            int col = curr[2];
 
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if (countLessEqual(matrix, mid) < k) {
-
-                low = mid + 1;
-
-            } else {
-
-                high = mid - 1;
+            ans = value;
+            if (col + 1 < n) {
+                minHeap.offer(
+                    new int[]{matrix[row][col + 1], row, col + 1}
+                );
             }
         }
-
-        return low;
+        return ans;
     }
 }
