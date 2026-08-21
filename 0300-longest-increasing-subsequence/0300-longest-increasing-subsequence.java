f227afdbf86e937a4c7e18b00fd8f5502a71d349
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-21 15:04:11 +0530

    Time: 453 ms (5.11%), Space: 273 MB (5.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
-
     public int lengthOfLIS(int[] nums) {
-
         int n = nums.length;
 
-        int[] dp = new int[n];
+        // prev can be -1, so shift it by +1
+        Integer[][] dp = new Integer[n][n + 1];
 
-        Arrays.fill(dp, 1);
+        return solve(nums, 0, -1, dp);
+    }
 
-        int ans = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            ans = Math.max(ans, dp[i]);
+    private int solve(int[] nums, int ind, int prev, Integer[][] dp) {
+
+        // Base case
+        if (ind == nums.length) {
+            return 0;
         }
-        return ans;
+
+        // prev + 1 because prev can be -1
+        if (dp[ind][prev + 1] != null) {
+            return dp[ind][prev + 1];
+        }
+
+        // Option 1: Don't take nums[ind]
+        int notTake = solve(nums, ind + 1, prev, dp);
+
+        // Option 2: Take nums[ind]
+        int take = 0;
+
+        if (prev == -1 || nums[ind] > nums[prev]) {
+            take = 1 + solve(nums, ind + 1, ind, dp);
+        }
+
+        return dp[ind][prev + 1] = Math.max(take, notTake);
     }
 }
