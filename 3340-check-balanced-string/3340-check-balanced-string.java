9783491d02388d3f0d9ce83c32e4aebcabf736d0
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-26 15:47:14 +0530

    Time: 1 ms (98.13%), Space: 43.5 MB (49.58%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int n = num.length();
+        int sumEven = 0;
+        int sumOdd = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sumEven += num.charAt(i) - '0';
+            } else {
+                sumOdd += num.charAt(i) - '0';
+            }
+        }
+        return sumEven == sumOdd;
+    }
+}
