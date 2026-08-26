e24ac5293b2622bc2681a475ae0660b182787d89
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-26 12:50:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.48%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,16 +15,34 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        dfs(root,ans);
-        return ans;
-    }
-     private void dfs(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
         }
-        dfs(root.left,ans);
-        dfs(root.right,ans);
-        ans.add(root.val);
+
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+
+        st1.push(root);
+
+        while (!st1.isEmpty()) {
+            TreeNode node = st1.pop();
+            st2.push(node);
+
+            if (node.left != null) {
+                st1.push(node.left);
+            }
+
+            if (node.right != null) {
+                st1.push(node.right);
+            }
+        }
+
+        while (!st2.isEmpty()) {
+            ans.add(st2.pop().val);
+        }
+
+        return ans;
     }
 }
