095c6ed36474090d0d631857936497529e5b1e2b
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-27 18:37:17 +0530

    Time: 13 ms (48.15%), Space: 44.3 MB (19.86%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        List<Integer> ans = new ArrayList<>();
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        makeParentMap(root, parent);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(target);
+
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+
+        int distance = 0;
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+
+            if (distance == k) {
+                break;
+            }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null &&
+                    !visited.contains(node.left)) {
+
+                    visited.add(node.left);
+                    q.offer(node.left);
+                }
+                if (node.right != null &&
+                    !visited.contains(node.right)) {
+
+                    visited.add(node.right);
+                    q.offer(node.right);
+                }
+                if (parent.containsKey(node) &&
+                    !visited.contains(parent.get(node))) {
+
+                    visited.add(parent.get(node));
+                    q.offer(parent.get(node));
+                }
+            }
+            distance++;
+        }
+        while (!q.isEmpty()) {
+            ans.add(q.poll().val);
+        }
+
+        return ans;
+    }
+
+    void makeParentMap(TreeNode root,
+                       Map<TreeNode, TreeNode> parent) {
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node.left != null) {
+                parent.put(node.left, node);
+                q.offer(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                q.offer(node.right);
+            }
+        }
+    }
+}
