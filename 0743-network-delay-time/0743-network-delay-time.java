34317a3976201c56c45fb528e8eb09c77e5d67c1
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-08-30 14:52:31 +0530

    Time: 10 ms (81.21%), Space: 49 MB (50.6%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -6,38 +6,26 @@ class Solution {
         for(int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-
         for(int[] edge : times) {
             graph[edge[0]].add(new int[]{edge[1], edge[2]});
         }
-
         int[] dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-
         PriorityQueue<int[]> pq =
             new PriorityQueue<>((a,b) -> a[0] - b[0]);
-
         dist[k] = 0;
         pq.offer(new int[]{0, k});
-
         while(!pq.isEmpty()) {
-
             int[] curr = pq.poll();
 
             int d = curr[0];
             int node = curr[1];
-
             if(d > dist[node]) continue;
-
             for(int[] nei : graph[node]) {
-
                 int next = nei[0];
                 int weight = nei[1];
-
                 if(dist[node] + weight < dist[next]) {
-
                     dist[next] = dist[node] + weight;
-
                     pq.offer(
                         new int[]{
                             dist[next],
@@ -47,17 +35,12 @@ class Solution {
                 }
             }
         }
-
         int ans = 0;
-
         for(int i = 1; i <= n; i++) {
-
             if(dist[i] == Integer.MAX_VALUE)
                 return -1;
-
             ans = Math.max(ans, dist[i]);
         }
-
         return ans;
     }
 }
