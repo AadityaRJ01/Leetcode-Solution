8bc11b0cbbc258a84050594b2f941291840b59eb
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-09-10 18:51:21 +0530

    Time: 2 ms (62.73%), Space: 43.4 MB (62.42%) - LeetHub

---
+++ 0593-valid-square/0593-valid-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        int[] d=new int[6];
+        d[0]=dist(p1,p2);
+        d[1]=dist(p1,p3);
+        d[2]=dist(p1,p4);
+        d[3]=dist(p2,p3);
+        d[4]=dist(p2,p4);
+        d[5]=dist(p3,p4);
+        Arrays.sort(d);
+
+        return d[0]>0 && d[0]==d[1] && d[1]==d[2] && d[2]==d[3] &&
+        d[4]==d[5] && d[3]<d[4];
+    }
+    private int dist(int[] p1,int[] p2){
+        int dx=p1[0]-p2[0];
+        int dy=p1[1]-p2[1];
+
+        return dx*dx+dy*dy;
+    }
+}
