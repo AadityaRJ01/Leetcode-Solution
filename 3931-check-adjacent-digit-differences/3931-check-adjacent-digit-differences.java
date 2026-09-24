e7cc9d46b82fa74ef8d4a0f06741058f7c332015
Aaditya Raj Joshi <aadityarajj3210@gmail.com>
2026-09-24 22:21:15 +0530

    Time: 1 ms (100%), Space: 43.7 MB (60%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        int n=s.length();
+        for(int i=0;i<n-1;i++){
+            int diff=Math.abs(s.charAt(i+1)-s.charAt(i));
+            if(diff>2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
